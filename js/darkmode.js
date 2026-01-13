a28b0c98f7e0bc625a16573631b45fa915b97d12
Thiago Lemos <lemost45@gmail.com>
2026-01-13 18:09:44 -0300

    ATUALIZANDO...

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,17 +1,13 @@
-// Pega o botão
 const btnDarkMode = document.getElementById('btnDarkMode');
 
-// Aplica dark mode se estiver salvo no localStorage
 if (localStorage.getItem('darkMode') === 'true') {
   document.body.classList.add('dark');
 }
 
-// Alterna dark mode ao clicar
-if(btnDarkMode){
+if (btnDarkMode) {
   btnDarkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark');
 
-    // Salva a preferência
     if (document.body.classList.contains('dark')) {
       localStorage.setItem('darkMode', 'true');
     } else {
