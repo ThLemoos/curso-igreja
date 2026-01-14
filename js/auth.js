0b3335c89771c292b124175532b4c7b41e9033b2
Thiago Lemos <lemost45@gmail.com>
2026-01-14 17:57:42 -0300

    atualizando...

--- js/auth.js
+++ js/auth.js
@@ -74,7 +74,7 @@ function login() {
   }
 
   localStorage.setItem("logado", email);
-  window.location.href = "../selecaoCurso/selecao.html";
+  window.location.href = "selecaoCurso/selecao.html";
 }
 
 function recuperar() {
